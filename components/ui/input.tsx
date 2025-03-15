8ea67d9341ce76baf87ba85c9421cb9e0a2aaf9c
tauhid97k <tauhid.coder@gmail.com>
2025-03-15 21:46:39 +0600

    adjusted input focus ring color

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 font-medium ring-offset-background transition-[color,box-shadow] file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-sm placeholder:text-muted-foreground focus-visible:border-primary/70 focus-visible:ring-2 focus-visible:ring-ring/30 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50 dark:focus-visible:ring-ring/40",
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 font-medium ring-offset-background transition-[color,box-shadow] file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-sm placeholder:text-muted-foreground focus-visible:border-primary/70 focus-visible:ring-2 focus-visible:ring-ring/30 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50 dark:focus-visible:ring-ring/20",
           className,
         )}
         ref={ref}
