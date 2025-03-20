003c6cdbcdd3162d03528cc5950eb777a7ef6034
tauhid97k <tauhid.coder@gmail.com>
2025-03-20 12:13:26 +0600

    refactored Next.js middleware and Prisma setup
    - Refactor middleware logic to potentially avoid redirect loops and other issues
    - Refactor Prisma setup to follow Next.js recommended practice

--- middleware.ts
+++ middleware.ts
@@ -6,32 +6,49 @@ export async function middleware(request: NextRequest) {
 
   const { pathname } = request.nextUrl;
 
-  const protectedRoute = pathname.startsWith("/dashboard");
-  const authRoute = pathname.startsWith("/auth");
+  /**
+   * Trailing slash is important in the route prefix.
+   * Prefix is defined to avoid manually declaring routes.
+   * RoutePrefix type ensures that the correct route prefix is used, preventing unintended access.
+   */
+  type RoutePrefix = `/${string}/`;
+  const authRoutePrefix: RoutePrefix[] = ["/auth/"];
+  const protectedRoutePrefix: RoutePrefix[] = ["/dashboard/"];
+
+  // Auth routes (e.g., /auth/login)
+  const isAuthRoute =
+    pathname == "/auth" ||
+    authRoutePrefix.some((prefix) => pathname.startsWith(prefix));
+
+  // Protected routes (e.g., /dashboard/settings)
+  const isProtectedRoute =
+    pathname == "/dashboard" ||
+    protectedRoutePrefix.some((prefix) => pathname.startsWith(prefix));
 
   /**
    * If not authenticated don't allow /dashboard pages
    * Redirect to /auth/sign-in
    */
-  if (protectedRoute) {
-    return session
-      ? NextResponse.next()
-      : NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  if (isProtectedRoute && !session) {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
   }
 
   /**
    * If authenticated don't allow /auth pages
    * Redirect to /dashboard
    */
-  if (authRoute) {
-    return session
-      ? NextResponse.redirect(new URL("/dashboard", request.url))
-      : NextResponse.next();
+  if (isAuthRoute && session) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/auth/:path*"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient({
   log: ["warn", "error"],
 });
+
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
