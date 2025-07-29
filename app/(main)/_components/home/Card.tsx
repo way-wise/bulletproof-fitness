f5173e497cdfd12e411b03a1ccab3c062eebd0b3
sakil-sagor <sakilsagor76543@gmail.com>
2025-07-29 17:22:23 +0600

    update home page

--- app/(main)/_components/home/Card.tsx
+++ app/(main)/_components/home/Card.tsx
@@ -1,5 +1,45 @@
-const Card = () => {
-  return <div>Card</div>;
+import { Badge } from "@/components/ui/badge";
+import { CardContent, Card as CardUI } from "@/components/ui/card";
+import { Eye, Heart, MessageCircle, Star } from "lucide-react";
+
+const Card = ({ item }: { item: any }) => {
+  return (
+    <div>
+      <CardUI className="overflow-hidden rounded-none border-none shadow-none">
+        <div className="relative aspect-video rounded-none shadow-none">
+          <iframe
+            src={item.videoUrl}
+            title={item.title}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="h-full w-full"
+          ></iframe>
+        </div>
+        <CardContent className="space-y-2 border-none p-4">
+          {item.category && (
+            <Badge className="rounded-none">{item.category}</Badge>
+          )}
+          <p className="text-sm text-gray-500">{item.equipment}</p>
+          <h3 className="text-lg font-bold uppercase">{item.title}</h3>
+          <div className="flex items-center gap-4 text-sm text-muted-foreground">
+            <span className="flex items-center gap-1">
+              <Eye className="h-4 w-4" /> {item.views}
+            </span>
+            <span className="flex items-center gap-1">
+              <Star className="h-4 w-4" /> {item.likes}
+            </span>
+            <span className="flex items-center gap-1">
+              <MessageCircle className="h-4 w-4" /> {item.comments}
+            </span>
+            <span className="flex items-center gap-1">
+              <Heart className="h-4 w-4" /> {item.saves}
+            </span>
+          </div>
+        </CardContent>
+      </CardUI>
+    </div>
+  );
 };
 
 export default Card;
--- app/(main)/_components/home/CardsSection.tsx
+++ app/(main)/_components/home/CardsSection.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { Badge } from "@/components/ui/badge";
-import { Card, CardContent } from "@/components/ui/card";
-import { Eye, Heart, MessageCircle, Star } from "lucide-react";
+import Card from "./Card";
 import FilterSection from "./FilterSection";
 const seedData = [
   {
@@ -37,44 +35,27 @@ const seedData = [
     saves: 1,
     videoUrl: "https://www.youtube.com/embed/W-nSrE8o1yJAk",
   },
+  {
+    id: 4,
+    title: "CONCENTRATION CURL",
+    category: "Biceps",
+    equipment: "Bulletproof Fitness Equipment",
+    views: 906,
+    likes: 0,
+    comments: 0,
+    saves: 1,
+    videoUrl: "https://www.youtube.com/embed/W-nSrE8o1yJAk",
+  },
 ];
 const CardsSection = () => {
   return (
-    <div>
-      <FilterSection />
-      <div className="mt-24 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="container mx-auto">
+      <div className="mt-24">
+        <FilterSection />
+      </div>
+      <div className="mt-24 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {seedData.map((item) => (
-          <Card key={item.id} className="overflow-hidden border">
-            <div className="relative aspect-video">
-              <iframe
-                src={item.videoUrl}
-                title={item.title}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                className="h-full w-full"
-              ></iframe>
-            </div>
-            <CardContent className="space-y-2 p-4">
-              {item.category && <Badge>{item.category}</Badge>}
-              <p className="text-sm text-gray-500">{item.equipment}</p>
-              <h3 className="text-lg font-bold uppercase">{item.title}</h3>
-              <div className="flex items-center gap-4 text-sm text-muted-foreground">
-                <span className="flex items-center gap-1">
-                  <Eye className="h-4 w-4" /> {item.views}
-                </span>
-                <span className="flex items-center gap-1">
-                  <Star className="h-4 w-4" /> {item.likes}
-                </span>
-                <span className="flex items-center gap-1">
-                  <MessageCircle className="h-4 w-4" /> {item.comments}
-                </span>
-                <span className="flex items-center gap-1">
-                  <Heart className="h-4 w-4" /> {item.saves}
-                </span>
-              </div>
-            </CardContent>
-          </Card>
+          <Card key={item.id} item={item} />
         ))}
       </div>
     </div>
