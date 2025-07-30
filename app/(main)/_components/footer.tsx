a6a23bf552fd2dae9d38d340eaa659ed38a72b7b
sakil-sagor <sakilsagor76543@gmail.com>
2025-07-30 18:41:04 +0600

    complete ui design

--- app/(main)/_components/footer.tsx
+++ app/(main)/_components/footer.tsx
@@ -1,9 +1,23 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="mt-auto border-t border-border bg-white py-3 dark:bg-card">
-      <div className="container">
+    <footer className="mt-auto border-t border-border bg-gray-700 py-3 dark:bg-card">
+      <div className="container flex items-center justify-center gap-2 text-xs md:text-sm">
         <p className="text-center">
           &copy;Copyright {new Date().getFullYear()} Company Name
+        </p>
+        <p>|</p>
+        <p className="text-center">
+          <Link href="/" className="hover:underline">
+            Privacy Policy
+          </Link>
+        </p>
+        <p>|</p>
+        <p>
+          <Link href="/" className="hover:underline">
+            Terms of Service
+          </Link>
         </p>
       </div>
     </footer>
