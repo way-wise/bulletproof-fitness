f78ad367938a9ba2c68fc089ec1d103fdc8bb7d9
rakib0101 <rakib.01jan@gmail.com>
2025-08-04 16:48:35 +0600

    test

--- app/api/features/exercise-library/exerciseLibraryModule.ts
+++ app/api/features/exercise-library/exerciseLibraryModule.ts
@@ -489,6 +489,8 @@ exerciseLibraryModule.post("/", async (c) => {
       data: await c.req.json(),
     });
 
+    console.log("Exercise Library validatedBody", validatedBody);
+
     const result =
       await exerciseLibraryService.createExerciseLibrary(validatedBody);
 
