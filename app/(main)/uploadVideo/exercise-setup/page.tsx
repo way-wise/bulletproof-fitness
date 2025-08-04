4d36ac3beabecafd2a813b0a1a39598dae5f7032
sakil-sagor <sakilsagor76543@gmail.com>
2025-08-04 12:43:53 +0600

    fix error for build

--- app/(main)/uploadVideo/exercise-setup/page.tsx
+++ app/(main)/uploadVideo/exercise-setup/page.tsx
@@ -1,5 +1,8 @@
 import ExerciseSetupVideoForm from "../../_components/upload-video/ExerciseSetupVideoForm";
 
+// Force dynamic rendering to prevent prerender errors
+export const dynamic = "force-dynamic";
+
 const ExerciseLibrary = () => {
   return (
     <div className="py-12">
