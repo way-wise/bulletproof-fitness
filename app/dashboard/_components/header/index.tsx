6ecb273630cb3286ad354e77fc7949079f660820
rakib0101 <rakib.01jan@gmail.com>
2025-08-07 22:01:47 +0600

    dashboard header fix

--- app/dashboard/_components/header/index.tsx
+++ app/dashboard/_components/header/index.tsx
@@ -12,7 +12,7 @@ const Header = async () => {
         <SidebarToggle />
       </div>
       <div className="flex items-center gap-4">
-        <ThemeSwitcher />
+        {/* <ThemeSwitcher /> */}
         <ProfileDropdown session={session} />
       </div>
     </header>
