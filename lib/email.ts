ce2a904c715e528ae7bc89bb1ef604e4591996b5
tauhid97k <tauhid.coder@gmail.com>
2025-10-16 21:53:15 +0600

    change nodeMailer preset to gmail service

--- lib/email.ts
+++ lib/email.ts
@@ -1,8 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST!,
-  port: parseInt(process.env.SMTP_PORT!),
+  // host: process.env.SMTP_HOST!,
+  // port: parseInt(process.env.SMTP_PORT!),
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER!,
     pass: process.env.SMTP_PASS!,
