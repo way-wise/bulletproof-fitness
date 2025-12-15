8c93609a712ad929787c44b8be9812a4a69a2a58
tauhid97k <tauhid.coder@gmail.com>
2025-12-15 16:53:22 +0600

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@/prisma/generated/client";
-
-const prisma = new PrismaClient();
+import prisma from "../lib/prisma"
 
 /**
  * Seed script for initializing permissions and roles
